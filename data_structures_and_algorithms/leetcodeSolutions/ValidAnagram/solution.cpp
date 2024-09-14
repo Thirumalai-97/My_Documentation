fa929ed9af83447d227c00e443a17128fe8f4978
Thirumalai <52080274+Thirumalai-97@users.noreply.github.com>
2024-09-14 13:11:10 +0530

    Update solution.cpp

--- data_structures_and_algorithms/leetcodeSolutions/ValidAnagram/solution.cpp
+++ data_structures_and_algorithms/leetcodeSolutions/ValidAnagram/solution.cpp
@@ -1,27 +1,14 @@
-Given two strings s and t, return true if t is an 
-anagram
- of s, and false otherwise.
+class Solution(object):
+    def isAnagram(self, s, t):
+        
+        if len(s)!=len(t):
+            return False
+        countS, countT ={},{}
 
- 
-
-Example 1:
-
-Input: s = "anagram", t = "nagaram"
-
-Output: true
-
-Example 2:
-
-Input: s = "rat", t = "car"
-
-Output: false
-
- 
-
-Constraints:
-
-1 <= s.length, t.length <= 5 * 104
-s and t consist of lowercase English letters.
- 
-
-Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+        for i in range(len(s)):
+            countS[s[i]]=1+countS.get(s[i],0)
+            countT[t[i]]=1+countT.get(t[i],0)
+        for c in countS:
+            if countS[c]!=countT.get(c,0):
+                return False
+        return True 
